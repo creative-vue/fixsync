5c2160475f94c96415da2f81818a83ca36b5023b
php4world <76422937@qq.com>
2021-02-19 11:26:28 +0800

    x

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// 模块“vscode”包含VS Code可扩展性API
+// 导入模块，并在下面的代码中使用别名vscode引用该模块
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// 激活您的扩展程序时将调用此方法
+// 您的扩展程序在第一次执行命令时被激活
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+	// 使用控制台输出诊断信息（console.log）和错误（console.error）
+	// 这行代码仅在激活您的扩展程序后才执行一次
 	console.log('Congratulations, your extension "vscode-sync" is now active!');
 
-	// The command has been defined in the package.json file
+	// 该命令已在package.json文件中定义
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-sync.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('vscode-sync.syncSettings', () => {
+		// 每次执行命令时都会执行您放置在此处的代码
+		vscode.workspace.getConfiguration().update('vscode-sync.username', '');
+		console.log(vscode.workspace.getConfiguration().get('vscode-sync.username'));
+		
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-sync!');
+		// 向用户显示消息框
+		// vscode.window.showInformationMessage('Hello World from vscode-sync!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
+// 停用您的扩展程序时调用此方法
 export function deactivate() {}
