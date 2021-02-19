6629cf79ff39bde8fb4955a7bc3c34b63328bb3b
php4world <76422937@qq.com>
2021-02-19 13:24:34 +0800

    rename

--- src/extension.ts
+++ src/extension.ts
@@ -8,23 +8,23 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// 使用控制台输出诊断信息（console.log）和错误（console.error）
 	// 这行代码仅在激活您的扩展程序后才执行一次
-	console.log('Congratulations, your extension "vscode-sync" is now active!');
+	console.log('Congratulations, your extension "fixsync" is now active!');
 
 	// 该命令已在package.json文件中定义
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-sync.syncSettings', () => {
+	let disposable = vscode.commands.registerCommand('fixsync.syncSettings', () => {
 		// 每次执行命令时都会执行您放置在此处的代码
-		vscode.workspace.getConfiguration().update('vscode-sync.username', '');
-		console.log(vscode.workspace.getConfiguration().get('vscode-sync.username'));
-		
+		vscode.workspace.getConfiguration().update('fixsync.username', '');
+		console.log(vscode.workspace.getConfiguration().get('fixsync.username'));
+
 
 		// 向用户显示消息框
-		// vscode.window.showInformationMessage('Hello World from vscode-sync!');
+		// vscode.window.showInformationMessage('Hello World from fixsync!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // 停用您的扩展程序时调用此方法
-export function deactivate() {}
+export function deactivate() { }
